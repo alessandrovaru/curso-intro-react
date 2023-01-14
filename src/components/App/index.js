510763fefdb9e994c2c36955dd4d992632a684d2
Alessandrovaru <alessandrovaru@gmail.com>
2023-01-13 21:01:22 -0400

    Local storage working

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -12,7 +12,7 @@ import { AppUI } from './AppUI';
 function App() {
   const localStorageTodos = localStorage.getItem('TODOS_V1');
   let parsedTodos;
-  
+
   if(!localStorageTodos) {
     localStorage.setItem('TODOS_V1', JSON.stringify([]));
     parsedTodos = [];
@@ -38,19 +38,25 @@ function App() {
     });
   };
 
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setTodos(newTodos);
+  }
+
   // Esto genera un re render
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1)
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   return (
